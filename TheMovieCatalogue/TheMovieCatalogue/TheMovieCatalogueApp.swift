8e746ce79900b99cd8dedb4f0093b3bf08f5202f
Gerardo Ramos <gerx_37@hotmail.com>
2023-06-15 12:01:25 -0600

    Created Xcode Project

---
+++ TheMovieCatalogue/TheMovieCatalogue/TheMovieCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieCatalogueApp.swift
+//  TheMovieCatalogue
+//
+//  Created by Gerardo Leal on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
