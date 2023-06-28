64672f31b19b1b404b6e9d2f1bab2b7d4398bf0f
Angel Avalos <leonavalos4772@gmail.com>
2023-06-27 19:06:16 -0600

    Changed requests from Viewmodel file to NetworkMonitor file
    There was a mistake on putting the request in the wrong file.

--- TheMovieCatalogue/TheMovieCatalogue/ViewModel/MovieViewModel.swift
+++ TheMovieCatalogue/TheMovieCatalogue/ViewModel/MovieViewModel.swift
@@ -7,38 +7,5 @@
 import Foundation
 
 
-func decodingProcess(from data: Data) {
-    let decoder = JSONDecoder()
-
-    do {
-        let movie = try decoder.decode(Movie.self, from: data)
-        print("Movie: \(movie)")
-
-//        if let imageUrl = URL(string: pokemon.sprites.frontDefault) {
-//            getImage(from: imageUrl)
-//        }
-
-//        let movesEndpoint: PokemonEndpoint = .move(pokemon.id)
-//        print(movesEndpoint.path)
-        
-    } catch let error {
-        print("Error: \(error)")
-    }
-}
-
-let session = URLSession.shared
-
-func fetchMovie(from url: URL) {
-    let urlRequest = URLRequest(url: url)
-    let task = session.dataTask(with: urlRequest) { data, _, error in
-        guard let data = data else {
-            print("Error: \(String(describing: error))")
-            return
-        }
-        decodingProcess(from: data)
-    }
-    task.resume()
-}
-
 
 
