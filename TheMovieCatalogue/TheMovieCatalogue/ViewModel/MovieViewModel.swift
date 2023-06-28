1191c6ccfb05d356951e1f7fe7e3309a782b9c3e
Gerardo Ramos <gerx_37@hotmail.com>
2023-06-28 14:19:55 -0600

    Fixed spaces

--- TheMovieCatalogue/TheMovieCatalogue/ViewModel/MovieViewModel.swift
+++ TheMovieCatalogue/TheMovieCatalogue/ViewModel/MovieViewModel.swift
@@ -5,3 +5,4 @@
 //  Created by Leo Avalos on 26/06/23.
 //
 import Foundation
+
--- TheMovieCatalogue/TheMovieCatalogue/Views/Main/ContentView.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Main/ContentView.swift
@@ -24,7 +24,6 @@ struct ContentView: View {
             } label: {
                 Text("Hola")
             }
-
         }
         .padding()
     }
