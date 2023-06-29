1fae3fe9ededaae088a5338ee1faa6ac84fba61b
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-06-29 12:12:49 -0700

    Created NewMovieCarrousel View

---
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Components/NewMovieCarrousel.swift
@@ -0,0 +1,20 @@
+//
+//  NewMovieCarrousel.swift
+//  TheMovieCatalogue
+//
+//  Created by Moises Lopez on 29/06/23.
+//
+
+import SwiftUI
+
+struct NewMovieCarrousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewMovieCarrousel_Previews: PreviewProvider {
+    static var previews: some View {
+        NewMovieCarrousel()
+    }
+}
