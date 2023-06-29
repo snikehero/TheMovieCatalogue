127f5e0db9e2a26d315d5826d9d25958a911a676
Gerardo Ramos <gerx_37@hotmail.com>
2023-06-29 12:37:38 -0600

    Update MovieViewModel

--- TheMovieCatalogue/TheMovieCatalogue/ViewModels/MovieViewModel.swift
+++ TheMovieCatalogue/TheMovieCatalogue/ViewModels/MovieViewModel.swift
@@ -5,11 +5,3 @@
 //  Created by Leo Avalos on 26/06/23.
 //
 import Foundation
-import SwiftUI
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 64672f3 (Changed requests from Viewmodel file to NetworkMonitor file)
