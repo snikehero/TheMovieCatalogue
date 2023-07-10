ebda40a1683c7e02f9ef71d22964f7ed4bd48456
Gerardo Ramos <gerx_37@hotmail.com>
2023-07-10 17:26:14 -0600

    Squashed commit of the following:
    commit 5b5813255fd29bc6770092e0ab7ba10f719ffe02
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Mon Jul 10 10:05:37 2023 -0600
    
        Added imageEndpoint for Movie and MovieListItem models
    
    commit 9646eb3ecb0853ea71d6c2509c029cbedaf488c7
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Mon Jul 10 09:21:27 2023 -0600
    
        Changed Populars model to MovieListPage
    
    commit b59aed97853180a1e1f617108654ab8fc47bbdbc
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Fri Jul 7 12:05:02 2023 -0600
    
        Eliminated magic numbers
    
    commit eca4d863dc88827ff8cf3b7e95e15ffbf7d22e66
    Author: Gerardo Ramos <gerx_37@hotmail.com>
    Date:   Fri Jul 7 08:32:01 2023 -0600
    
        Fetches Populars for Carrousel View
    
        - CarrouselView now receives an array of MovieListItem
        - Created MovieListItem mock

--- TheMovieCatalogue/TheMovieCatalogue/Views/Components/CarrouselView.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Components/CarrouselView.swift
@@ -8,65 +8,75 @@
 import SwiftUI
 
 struct CarrouselView: View {
-  let newMovies:[Int] = Array(0...9)
-  let popularMovies:[Int] = Array(0...9)
-  
-  var body: some View {
-    NavigationStack {
-      ScrollView {
-        VStack(spacing: Constants.General.spacing) {
-          HorizontalCarrouselView(title: "New Movies >",
-                                  newMovies: newMovies,
-                                  navigationLinkDestination: AnyView(NewMoviesView())
-          )
-          HorizontalCarrouselView(title: "Popular Movies >",
-                                  newMovies: popularMovies,
-                                  navigationLinkDestination: AnyView(PopularMoviesView())
-          )
-          Spacer()
+    let newMovies: [MovieListItem]
+    let popularMovies: [MovieListItem]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: Constants.General.spacing) {
+                    HorizontalCarrouselView(title: "New Movies >",
+                                            newMovies: newMovies,
+                                            navigationLinkDestination: AnyView(NewMoviesView())
+                    )
+                    HorizontalCarrouselView(title: "Popular Movies >",
+                                            newMovies: popularMovies,
+                                            navigationLinkDestination: AnyView(PopularMoviesView())
+                    )
+                    Spacer()
+                }
+                .padding()
+            }
         }
-        .padding()
-      }
     }
-  }
 }
 
 private struct HorizontalCarrouselView: View {
-  let title: String
-  let newMovies: [Int]
-  let navigationLinkDestination: AnyView
-  
-  var body: some View {
-    VStack(alignment: .leading,spacing: Constants.CarrouselImages.spacing) {
-      NavigationLink {
-        navigationLinkDestination
-      } label: {
-        Text(title)
-          .font(.system(.title2, design: .rounded))
-          .accentColor(.primary)
-      }
-      ScrollView(.horizontal) {
-        HStack(spacing: Constants.CarrouselImages.spacing) {
-          ForEach(newMovies, id: \.self) { movie in
+    let title: String
+    let newMovies: [MovieListItem]
+    let navigationLinkDestination: AnyView
+    
+    var body: some View {
+        VStack(alignment: .leading,spacing: Constants.CarrouselImages.spacing) {
             NavigationLink {
-              EmptyView()
+                navigationLinkDestination
             } label: {
-              Image("Batman")
-                .resizable()
-                .frame(width: Constants.CarrouselImages.width,
-                       height: Constants.CarrouselImages.height)
-                .scaledToFill()
-                .clipShape(RoundedRectangle(cornerRadius: Constants.CarrouselImages.cornerRadius))
+                Text(title)
+                    .font(.system(.title2, design: .rounded))
+                    .accentColor(.primary)
             }
-          }
+            ScrollView(.horizontal) {
+                HStack(spacing: Constants.CarrouselImages.spacing) {
+                    ForEach(newMovies.prefix(10)) { movie in
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            AsyncImage(
+                                url: URL(string: movie.poster),
+                                content: { image in
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: Constants.CarrouselImages.width, height: Constants.CarrouselImages.height)
+                                        .clipShape(RoundedRectangle(cornerRadius: Constants.CarrouselImages.cornerRadius))
+                                },
+                                placeholder: {
+                                    ProgressView()
+                                }
+                            )
+                            .frame(width: Constants.CarrouselImages.width,
+                                   height: Constants.CarrouselImages.height)
+                            .scaledToFill()
+                            .clipShape(RoundedRectangle(cornerRadius: Constants.CarrouselImages.cornerRadius))
+                        }
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
 
 struct CarrouselView_Previews: PreviewProvider {
   static var previews: some View {
-    CarrouselView()
+      CarrouselView(newMovies: [MovieListItem.mock, MovieListItem.mock], popularMovies: [MovieListItem.mock, MovieListItem.mock])
   }
 }
