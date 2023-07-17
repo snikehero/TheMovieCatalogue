41f1fdeb02711e48a661a818b0c4d2d22909ce55
ulises tapia <ulisestapiaburns@gmail.com>
2023-07-17 20:26:59 +0000

    Merged in THEM-120-Standardize-measurements-sizes-strings (pull request #18)
    THEM-120 Standardize measurements sizes strings
    
    * THEM-120 Standardize measurements, sizes and strings
    
    Create an enum Constants in a file.
    it should contain constant values for:
    width values
    height values
    Strings used in views
    change font sizes to apple given system sizes
    
    * Linked Files.
    
    * Corrections.
    
    Sinopsys to Synopsis
    
    * Fixed prurals
    
    
    Approved-by: Rubikangel
    Approved-by: Ivan Javier Aguilar Sajaropulos
    Approved-by: gerardo ramos leal
    Approved-by: Sandra Herrera

--- TheMovieCatalogue/TheMovieCatalogue/Views/Main/MovieDetailsView.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Main/MovieDetailsView.swift
@@ -8,46 +8,24 @@
 import SwiftUI
 
 struct MovieDetailsView: View {
-    
+    var movie: Movie = Movie.mock
     var body: some View {
         ScrollView {
             ZStack {
-                MovieDetails()
+                VStack{
+                    MovieImage(imageURL:  movie.backdrop)
+                    MovieTitle(text: movie.title)
+                    MovieSpecs()
+                    MovieSynopsis(text: movie.overview)
+                }
             }
         }
         .ignoresSafeArea()
     }
 }
 
-struct favoritesButton: View {
-    
-    var body: some View {
-        Button(action: {
-        }) {
-            Label("Add To Favorites", systemImage:"heart")
-        }
-        .frame(width: 300, height: 70)
-        .background(.ultraThinMaterial)
-        .clipShape(RoundedRectangle(cornerRadius: 20))
-    }
-}
-
-struct MovieTitle: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-            .font(.title)
-            .bold()
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.init(top: 0, leading: 12, bottom: 0, trailing: 12))
-            .lineLimit(1)
-    }
-}
-
 struct MovieSpecs: View {
     var movie: Movie = Movie.mock
-    
     var body: some View {
         HStack{
             MovieText(text: movie.genres[0].name)
@@ -56,65 +34,10 @@ struct MovieSpecs: View {
             Spacer()
             MovieText(text: "\(movie.runtime) min")
         }
-        .padding(.init(top: 0, leading: 12, bottom: 0, trailing: 12))
-    }
-}
-
-struct MovieDetails: View {
-    var movie: Movie = Movie.mock
-    
-    var body: some View {
-        VStack{
-            MovieImage(imageURL:  movie.backdrop)
-            MovieTitle(text: movie.title)
-            MovieSpecs()
-            MovieSynopsis(text: movie.overview)
-        }
-    }
-}
-
-struct MovieImage: View {
-    var imageURL: String
-    
-    var body: some View {
-        AsyncImage(
-            url: URL(string: imageURL),
-            content: { image in
-                image.resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 400, height: 620)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-            },
-            placeholder: {
-                ProgressView()
-            }
-        )
-        .frame(width: 400, height: 620)
-        .cornerRadius(20)
-        .overlay(alignment: .bottom) {
-            favoritesButton()
-                .padding()
-        }
-    }
-}
-
-struct MovieText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-            .font(.system(size: 18, weight:.regular, design: .default))
-    }
-}
-
-struct MovieSynopsis: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-            .padding(.init(top: 1, leading: 12, bottom: 0, trailing: 12))
-            .font(.system(size: 14, weight:.regular, design: .rounded))
-            .lineLimit(6)
+        .padding(.init(top: ConstantText.zeroText, leading: ConstantText.leadingText, bottom: ConstantText.zeroText, trailing: ConstantText.trailingText))
+        .background(Color(ColorConstant.movieDetailsBackgroundColor))
+        .clipShape(Capsule())
+        .padding(.horizontal)
     }
 }
 
