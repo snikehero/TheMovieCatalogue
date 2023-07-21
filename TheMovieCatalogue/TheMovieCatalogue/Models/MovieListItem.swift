2a5d1eed66c0c86f5a9fdaa668ac925fb6b1c915
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-07-21 09:07:42 -0700

    Created MovieListItem Mocks
    I used the json from the api and a json dcoder to get posterpaths from random movies, to make 7 MovieListItem mocks

--- TheMovieCatalogue/TheMovieCatalogue/Models/MovieListItem.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Models/MovieListItem.swift
@@ -21,4 +21,11 @@ struct MovieListItem: Identifiable, Codable, Hashable {
         case posterPath = "poster_path"
     }
     static let mock = MovieListItem(id: 268, posterPath: "/cij4dd21v2Rk2YtUQbV5kW69WB2.jpg")
+    static let mock2 = MovieListItem(id: 129, posterPath: "/gPbM0MK8CP8A174rmUwGsADNYKD.jpg")
+    static let mock3 = MovieListItem(id: 112, posterPath: "/hOb6ODI7QQFKkOe3eJU2Fdh2fk1.jpg")
+    static let mock4 = MovieListItem(id: 113, posterPath: "/NNxYkU70HPurnNCSiCjYAmacwm.jpg")
+    static let mock5 = MovieListItem(id: 114, posterPath: "/8riWcADI1ekEiBguVB9vkilhiQm.jpg")
+    static let mock6 = MovieListItem(id: 115, posterPath: "/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg")
+    static let mock7 = MovieListItem(id: 116, posterPath: "/nb9fc9INMg8kQ8L7sE7XTNsZnUX.jpg")
+
 }
