91a2dc4ddd39b1dbc6555bbeb8c39bab2ee8fffd
Gerardo Ramos <gerx_37@hotmail.com>
2023-07-28 10:17:15 -0600

    Changed MovieListItem model to have optional

--- TheMovieCatalogue/TheMovieCatalogue/Models/MovieListItem.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Models/MovieListItem.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct MovieListItem: Identifiable, Codable, Hashable {
     let id: Int
-    let posterPath: String
+    let posterPath: String?
     var posterSize: ImageSize = .width154
     var posterString: String {
         let endpointBuilder = EndpointBuilder()
-        return endpointBuilder.getImageURLString(imageSize: posterSize, imagePath: posterPath) ?? ""
+        return endpointBuilder.getImageURLString(imageSize: posterSize, imagePath: posterPath ?? "") ?? ""
     }
 
     enum CodingKeys: String, CodingKey {
