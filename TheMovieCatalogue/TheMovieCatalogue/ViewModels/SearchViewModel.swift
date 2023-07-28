d62f9d7d5c40960a7cada9a94982de62f3c9d9d8
Gerardo Ramos <gerx_37@hotmail.com>
2023-07-28 10:17:38 -0600

    Added searchResults to searchViewModel

--- TheMovieCatalogue/TheMovieCatalogue/ViewModels/SearchViewModel.swift
+++ TheMovieCatalogue/TheMovieCatalogue/ViewModels/SearchViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 @MainActor class SearchViewModel: ObservableObject {
 
     @Published var genres : [Genre] = []
+    @Published var searchResults : [MovieListItem] = []
 
     private var networkManager = NetworkManager()
     private let endpointBuilder = EndpointBuilder()
@@ -19,6 +20,17 @@ import Foundation
             if let genreListPage = genreListPage {
                 DispatchQueue.main.async { [weak self] in
                     self?.genres = genreListPage.genres
+                }
+            }
+        }
+    }
+
+    func fetchSearchList(search: String) {
+        networkManager.fetchData(endpoint: endpointBuilder.getMovieListBySearch(searchText: search),
+                                 type: MovieListPage.self) { coincidences in
+            if let coincidences = coincidences {
+                DispatchQueue.main.async { [weak self] in
+                    self?.searchResults = coincidences.results
                 }
             }
         }
