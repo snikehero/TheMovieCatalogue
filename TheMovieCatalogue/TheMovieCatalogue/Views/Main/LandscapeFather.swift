0f46999458e2dbe295c9972ee53083cba2c1095d
UBurns <133381906+UBurns@users.noreply.github.com>
2023-08-08 11:40:03 -0700

    Added LandscapeFather File
    Added LandscapeFather file to manage the logic between the landscape mode and portrait mode of MainView

---
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Main/LandscapeFather.swift
@@ -0,0 +1,27 @@
+//
+//  LandscaoeFather.swift
+//  TheMovieCatalogue
+//
+//  Created by Ulises Burns on 07/08/23.
+//
+
+import SwiftUI
+
+struct LandscapeFather: View {
+    @ObservedObject var mainViewModel : MainViewModel
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass!
+    var body: some View {
+        Group {
+            if verticalSizeClass == .compact {
+                MainViewLandscape_(mainViewModel: mainViewModel)
+            } else {
+                MainView(mainViewModel: mainViewModel)
+            }
+        }
+        }
+    }
+struct LandscapeFather_Previews: PreviewProvider {
+    static var previews: some View {
+        LandscapeFather(mainViewModel: MainViewModel())
+    }
+}
