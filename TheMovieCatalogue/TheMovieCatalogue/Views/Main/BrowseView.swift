f594ccfc118ddc36470a550a1bd3ebf4d2ef23e9
UBurns <133381906+UBurns@users.noreply.github.com>
2023-08-14 08:31:36 -0700

    Revert "Revert "Fixed Conflicts""
    This reverts commit 7715447f9f94cfed279a191f981d31e806131868.

--- TheMovieCatalogue/TheMovieCatalogue/Views/Main/BrowseView.swift
+++ TheMovieCatalogue/TheMovieCatalogue/Views/Main/BrowseView.swift
@@ -39,8 +39,15 @@ struct BrowseView: View {
             }
         }
         .fullScreenCover(isPresented: $showingFullScreenCover) {
-            ModularMovieListView(title: searchTerm,
-                                 moviesForFill: searchViewModel.searchResults, showBackButtonState: true)
+            ModularMovieListView(
+                showBackButtonState: true,
+                modularMovieListViewModel: ModularMovieListViewModel(
+                    title: searchTerm,
+                    withView: .search,
+                    networkManager: searchViewModel.networkManager,
+                    endpointBuilder: searchViewModel.endpointBuilder
+                )
+            )
         }
     }
 }
